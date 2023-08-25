fc07e52bc5aada3feab2a63bbf3fec02468c59c9
Hatsune Miku <l2a1knla@gmail.com>
2023-08-25 19:27:13 -0230

    Impl safe math operations

--- MultiflowLibrary/math/math.cpp
+++ MultiflowLibrary/math/math.cpp
@@ -6,88 +6,162 @@
 
 namespace math
 {
+constexpr double ZERO_THRESHOLD = 1e-5;
+
+#define ASSERT_VALID(x) \
+    if (std::isnan(x) || std::isinf(x)) \
+    { \
+        throw std::exception("数值非法，不可为NaN或无穷大"); \
+    }
+
+bool z(double x)
+{
+    ASSERT_VALID(x);
+    return std::abs(x) < ZERO_THRESHOLD;
+}
+
+bool eq(double x, double y)
+{
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
+    return z(x - y);
+}
+
+bool gt(double x, double y)
+{
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
+    return x > y + ZERO_THRESHOLD;
+}
+
+bool lt(double x, double y)
+{
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
+    return x < y - ZERO_THRESHOLD;
+}
+
+bool gte(double x, double y)
+{
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
+    return gt(x, y) || eq(x, y);
+}
+
+bool lte(double x, double y)
+{
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
+    return lt(x, y) || eq(x, y);
+}
+
 double add(double x, double y)
 {
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
     return x + y;
 }
 
 double subtract(double x, double y)
 {
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
     return x - y;
 }
 
 double multiply(double x, double y)
 {
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
+
+    // 避免超大的数和带有误差的0相乘不等于0的情况
+    if (z(x) || z(y))
+    {
+        return 0;
+    }
     return x * y;
 }
 
 double divide(double x, double y)
 {
-    if (y == 0)
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
+
+    // 避免带有误差的0除以极小的数不等于0的情况
+    if (z(x))
     {
-        throw std::exception("Divide by zero.");
+        return 0;
+    }
+
+    if (z(y))
+    {
+        throw std::exception("不可除以0");
     }
     return x / y;
 }
 
 double power(double x, double y)
 {
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
+
+    if (lt(x, 0) && lt(y, 1))
+    {
+        throw std::exception("不可对负数开方");
+    }
     return std::pow(x, y);
 }
 
 double square_root(double x)
 {
+    ASSERT_VALID(x);
+
+    if (lt(x, 0))
+    {
+        throw std::exception("实数不可<0");
+    }
     return std::sqrt(x);
 }
 
 double absolute_value(double x)
 {
+    ASSERT_VALID(x);
     return std::abs(x);
-}
-
-double sine(double x)
-{
-    return std::sin(x);
-}
-
-double cosine(double x)
-{
-    return std::cos(x);
-}
-
-double tangent(double x)
-{
-    return std::tan(x);
-}
-
-double arcsine(double x)
-{
-    return std::asin(x);
-}
-
-double arccosine(double x)
-{
-    return std::acos(x);
-}
-
-double arctangent(double x)
-{
-    return std::atan(x);
 }
 
 double logarithm(double base, double x)
 {
-    // 换底公式
-    return std::log(x) / std::log(base);
+    ASSERT_VALID(base);
+    ASSERT_VALID(x);
+
+    if (x <= 0)
+    {
+        throw std::exception("实数不可<=0");
+    }
+
+    if (eq(base, 1) || lte(base, 0))
+    {
+        throw std::exception("底数不能为1或<=0");
+    }
+
+    // 自带的std::log不知道是什么底数
+    // 用换底公式确保底数为base
+    // 由于之前检查过base不为1，所以不用担心出现除0
+    return divide(std::log(x), std::log(base));
 }
 
 double minimum(double x, double y)
 {
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
     return std::min(x, y);
 }
 
 double maximum(double x, double y)
 {
+    ASSERT_VALID(x);
+    ASSERT_VALID(y);
     return std::max(x, y);
 }
+
 }
--- MultiflowLibrary/math/math.h
+++ MultiflowLibrary/math/math.h
@@ -3,24 +3,99 @@
 
 #include "../MultiflowLibrary_global.h"
 
+/**
+ * @brief 基本数学操作。包含了一系列保护措施，
+ * 遇到各种非法操作会抛出异常
+ * 遇到NaN和Infinite也会抛出异常
+ */
 namespace math
 {
 
+/**
+ * @brief 判断x是否为零，兼容IEEE浮点数的误差
+ */
+ML_PUBLIC bool z(double x);
+
+/**
+ * @brief 判断两数是否相等，兼容IEEE浮点数的误差
+ */
+ML_PUBLIC bool eq(double x, double y);
+
+/**
+ * @brief 判断x是否大于y，兼容IEEE浮点数的误差
+ */
+ML_PUBLIC bool gt(double x, double y);
+
+/**
+ * @brief 判断x是否小于y，兼容IEEE浮点数的误差
+ */
+ML_PUBLIC bool lt(double x, double y);
+
+/**
+ * @brief 判断x是否大于等于y，兼容IEEE浮点数的误差
+ */
+ML_PUBLIC bool gte(double x, double y);
+
+/**
+ * @brief 判断x是否小于等于y，兼容IEEE浮点数的误差
+ */
+ML_PUBLIC bool lte(double x, double y);
+
+/**
+ * @brief 加法，不带溢出检测
+ */
 ML_PUBLIC double add(double x, double y);
+
+/**
+ * @brief 减法，不带溢出检测
+ */
 ML_PUBLIC double subtract(double x, double y);
+
+/**
+ * @brief 乘法，不带溢出检测
+ * 能够正确处理误差0和无穷大的情况
+ */
 ML_PUBLIC double multiply(double x, double y);
+
+/**
+ * @brief 除法，不带溢出检测
+ * 能够正确处理被除数是误差0、出现无穷大的情况
+ * 遇到除以0会抛出异常
+ */
 ML_PUBLIC double divide(double x, double y);
+
+/**
+ * @brief 幂函数，不带溢出检测
+ * 遇到对负数开方会抛出异常，即使有时候负数开方也是能开的
+ */
 ML_PUBLIC double power(double x, double y);
+
+/**
+ * @brief 平方根，遇到负数会抛出异常
+ */
 ML_PUBLIC double square_root(double x);
+
+/**
+ * @brief 绝对值
+ */
 ML_PUBLIC double absolute_value(double x);
-ML_PUBLIC double sine(double x);
-ML_PUBLIC double cosine(double x);
-ML_PUBLIC double tangent(double x);
-ML_PUBLIC double arcsine(double x);
-ML_PUBLIC double arccosine(double x);
-ML_PUBLIC double arctangent(double x);
+
+/**
+ * @brief 对数函数，不带溢出检测，遇到以下情况会抛出异常：
+ * base <= 0,
+ * base == 1, 或
+ * x <= 0
+ */
 ML_PUBLIC double logarithm(double base, double x);
+
+/**
+ * @brief 取较小值
+ */
 ML_PUBLIC double minimum(double x, double y);
+
+/**
+ * @brief 取较大值
+ */
 ML_PUBLIC double maximum(double x, double y);
 
 }
